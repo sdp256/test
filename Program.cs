5ec33caf3948254bf14d2e9d8fde51676844fde8
sdp256 <steevnpairy@gmail.com>
2021-01-29 17:50:39 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 8;
+            int y = 4;
+
+            if (x > y)
+            {
+                Console.WriteLine("x is greater than y");
+            }
+        }
+    }
+}
